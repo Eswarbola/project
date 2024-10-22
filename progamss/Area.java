d4d7503292c2aa5650425fa826c31d158bd32840
Eswarbola <228a1a4436@risekrishnasaiprakasam.edu.in>
2024-10-22 15:46:13 +0530

    Update Area.java

--- progamss/Area.java
+++ progamss/Area.java
@@ -1,12 +1,11 @@
 class Area
 {
- public static void main(String args[])
+ public static void main(String[] args)
  {
   float r=4.57f;
   float c;
   c=3.14f * r * r;
   System.out.println("area of circle is : "+c);
-  
   int l=7,b=9;
   int s;
   s=l*b;
